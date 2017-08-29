83014ca0041d5fbf1ff54e3af671c041e2e077c5
vikash kumar <iit2016041@iiita.ac.in>
2017-08-29 20:58:58 +0530

    Update todo.js

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -4,3 +4,10 @@ $("li").click(function(){
 	//alert("Clicked LI");
 	$(this).css("color", "red");
 });
+//click X to delete
+$("span").click(function(event){
+	$(this).parent().fadeOut().remove();
+	alert("Deleated");
+	event.stopPropagation();
+
+});
